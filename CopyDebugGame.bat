b6b70706efb597abeb3a0750bb5d7a7b0c671d89
1097195326 <1097195326@qq.com>
2020-09-18 10:08:28 +0800

    save map

---
+++ CopyDebugGame.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+set UE4RootPath=D:\UnrealEngine-4.24.2-release\
+set ProjectRootPath=D:\SaveMap\
+set PackageRootPath=E:\WindowsNoEditor\
+
+set UE4BinPath=%UE4RootPath%Engine\Binaries\Win64\
+set UE4PluginPath=%UE4RootPath%Engine\Plugins\
+set ProjectBinPath=%ProjectRootPath%Binaries\Win64\
+set ProjectPluginPath=%ProjectRootPath%Plugins\
+set PackageEngineBinPath=%PackageRootPath%Engine\Binaries\Win64\
+set PackageEnginePluginPath=%PackageRootPath%Engine\Plugins\
+set PackageBinPath=%PackageRootPath%SaveMap\Binaries\Win64\
+set PackagePluginPath=%PackageRootPath%SaveMap\Plugins\
+
+xcopy /e /y %ProjectBinPath%UE4Editor-SaveMap-Win64-Debug.dll		 		%PackageBinPath%
+xcopy /e /y %ProjectBinPath%UE4Editor-SaveMap-Win64-Debug.pdb		 		%PackageBinPath%
+
+pause
---
+++ Source/SaveMap/SaveMapInstance.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SaveMapInstance.generated.h"
+
+UCLASS()
+class USaveMapInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	USaveMapInstance();
+
+	UFUNCTION(Exec)
+		void TestSave();
+	UFUNCTION(Exec)
+		void TravelMap();
+	//Begin Exec system
+	/** delegate type for pre save world events ( uint32 SaveFlags, UWorld* World ) */
+	DECLARE_MULTICAST_DELEGATE_TwoParams(FOnPreSaveWorld, uint32, class UWorld*);
+	/** delegate type for post save world events ( uint32 SaveFlags, UWorld* World, bool bSuccess ) */
+	DECLARE_MULTICAST_DELEGATE_ThreeParams(FOnPostSaveWorld, uint32, class UWorld*, bool);
+
+	/** Called before SaveWorld is processed */
+	static FOnPreSaveWorld PreSaveWorld;
+	/** Called after SaveWorld is processed */
+	static FOnPostSaveWorld PostSaveWorld;
+
+	virtual bool Exec(UWorld* InWorld, const TCHAR* Cmd, FOutputDevice& Out = *GLog) override;
+	bool Exec_Obj(const TCHAR* Str, FOutputDevice& Ar);
+
+	bool SaveCurrentLevel();
+
+	bool SaveMap(UWorld* World, const FString& Filename);
+
+	bool SaveAsImplementation(UWorld* InWorld, const FString& DefaultFilename, const bool bAllowStreamingLevelRename, FString* OutSavedFilename);
+	
+	bool SaveWorld(UWorld* World,
+		const FString* ForceFilename,
+		const TCHAR* OverridePath,
+		const TCHAR* FilenamePrefix,
+		bool bRenamePackageToFile,
+		bool bCheckDirty,
+		FString& FinalFilename,
+		bool bAutosaving,
+		bool bPIESaving);
+
+	/** The editor wrapper for UPackage::SavePackage. Auto-adds files to source control when necessary */
+	bool SavePackage(UPackage* InOuter, UObject* Base, EObjectFlags TopLevelFlags, const TCHAR* Filename,
+		FOutputDevice* Error = GError, FLinkerNull* Conform = NULL, bool bForceByteSwapping = false, bool bWarnOfLongFilename = true,
+		uint32 SaveFlags = SAVE_None, const class ITargetPlatform* TargetPlatform = NULL, const FDateTime& FinalTimeStamp = FDateTime::MinValue(), bool bSlowTask = true);
+
+	/** The editor wrapper for UPackage::Save. Auto-adds files to source control when necessary */
+	FSavePackageResultStruct Save(UPackage* InOuter, UObject* Base, EObjectFlags TopLevelFlags, const TCHAR* Filename,
+		FOutputDevice* Error = GError, FLinkerNull* Conform = NULL, bool bForceByteSwapping = false, bool bWarnOfLongFilename = true,
+		uint32 SaveFlags = SAVE_None, const class ITargetPlatform* TargetPlatform = NULL, const FDateTime& FinalTimeStamp = FDateTime::MinValue(),
+		bool bSlowTask = true, class FArchiveDiffMap* InOutDiffMap = nullptr,
+		FSavePackageContext* SavePackageContext = nullptr);
+
+	/** Invoked before a UWorld is saved to update editor systems */
+	void OnPreSaveWorld(uint32 SaveFlags, UWorld* World);
+
+	/** Invoked after a UWorld is saved to update editor systems */
+	void OnPostSaveWorld(uint32 SaveFlags, UWorld* World, uint32 OriginalPackageFlags, bool bSuccess);
+
+	bool DoesPackageExist(UPackage* Package, FString* OutFilename = nullptr);
+
+	bool InitializePhysicsSceneForSaveIfNecessary(UWorld* World, bool &bOutForceInitialized);
+	void CleanupPhysicsSceneThatWasInitializedForSave(UWorld* World, bool bForceInitialized);
+
+	bool CanSavePackage(UPackage* PackageToSave);
+	UWorld::InitializationValues GetEditorWorldInitializationValues() const;
+	FString GetAutoSaveDir();
+	//End Exec System
+
+};
